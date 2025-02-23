7f1332b796744b854aed5587cbc1a52772ec9cee
Jing <jingli0319@gmail.com>
2025-02-24 00:05:54 +0100

    downgrade to react 17

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import React from "react";
+import ReactDOM from "react-dom"; // React 17 nutzt diesen Import
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
