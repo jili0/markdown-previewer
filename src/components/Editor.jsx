82a02108d23c2c88a5c2a912e0c21d473358cdd1
Jing <jingli0319@gmail.com>
2025-02-24 01:28:06 +0100

    set the height of textarea dynamically

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,18 +1,33 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import { AppContext } from "../AppContextProvider";
 
 const Editor = () => {
   const { markdown, setMarkdown } = useContext(AppContext);
+  const textareaRef = useRef();
+
+  useEffect(() => {
+    // Adjust height on initial load
+    if (textareaRef.current) {
+      textareaRef.current.style.height = `${
+        textareaRef.current.scrollHeight + 100
+      }px`;
+    }
+  }, []);
+
   const handleChange = (e) => {
     setMarkdown(e.target.value);
+    const textarea = e.target;
+    textarea.style.height = "auto"; // Reset height to auto before recalculating
+    textarea.style.height = `${textarea.scrollHeight}px`; // Set height based on content
   };
   return (
     <div className="editor">
       <label htmlFor="editor" className="header">
-      ✍️ Editor
+        ✍️ Editor
       </label>
       <textarea
         id="editor"
+        ref={textareaRef}
         onChange={handleChange}
         value={markdown}
         rows="20"
